e9e0bd57b8a31168acb13e8aa628da2223e35618
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-10-04 12:39:26 +0300

    admin - xyinya

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,5 @@ app.use('/questions', require('./routes/questionRoutes'));
 app.use('/answers', require('./routes/answerRoutes'));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
